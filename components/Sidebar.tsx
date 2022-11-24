145ec44b2ae10eff0e2f9ccc274f8cfd830f773a
alvin <alvine@gmail.com>
2022-11-24 10:34:53 +0700

    setup layout

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,10 +1,14 @@
+import Image from 'next/image'
 import React from 'react'
+import { logo } from '../assets'
 
 export default function Sidebar() {
   return (
     <div>
       <div className='sidebar'>
-
+        <div className='pl-5 pt-3'>
+          <Image src={logo} alt='logo multiverse' width={100} height={100} />
+        </div>
       </div>
     </div>
   )
