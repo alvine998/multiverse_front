64f0ce95733446162e000281a03c261c625863af
alvin <alvinecom2018@gmail.com>
2022-12-09 20:09:36 +0700

    --

--- pages/index.tsx
+++ pages/index.tsx
@@ -278,16 +278,16 @@ export default function Home() {
                 <Image src={connects} alt="connect" width={250} height={150} style={{ borderRadius: '20px' }} />
                 <h5 className='mt-3'>Follow Us!</h5>
                 <div className='d-flex mt-3 justify-content-around'>
-                  <Link href={'whatsapp://send?abid=628879567888&text=Hello%2C%20World!'}>
+                  <Link href={'whatsapp://send?abid=628879567888&text=Hello%2C%20World!'} rel="noopener">
                     <Image src={whatsapp2} alt="connect" width={35} height={35} style={{ borderRadius: '20px' }} />
                   </Link>
-                  <Link target={'_blank'} href={'mailto:sales@ptmultiverse.com'}>
+                  <Link target={'_blank'} href={'mailto:sales@ptmultiverse.com'} rel="noopener">
                     <Image src={email2} alt="connect" width={35} height={35} style={{ borderRadius: '20px' }} />
                   </Link>
-                  <Link target={'_blank'} href={'https://instagram.com/'}>
+                  <Link target={'_blank'} href={'https://instagram.com/'} rel="noopener">
                     <Image src={instagram} alt="connect" width={35} height={35} style={{ borderRadius: '20px' }} />
                   </Link>
-                  <Link target={'_blank'} href={'https://www.google.com/maps/place/PT+UNIVERSE+SOLUSI+DIGITAL/@-6.1077868,106.6180349,12z/data=!4m10!1m2!2m1!1spt+multiverse!3m6!1s0x2e69f5caa2f92667:0x8ffaa52281733371!8m2!3d-6.2080777!4d106.8204522!15sCg1wdCBtdWx0aXZlcnNlkgEQc29mdHdhcmVfY29tcGFueeABAA!16s%2Fg%2F11jt04mf8x'}>
+                  <Link target={'_blank'} href={'https://www.google.com/maps/place/PT+UNIVERSE+SOLUSI+DIGITAL/@-6.1077868,106.6180349,12z/data=!4m10!1m2!2m1!1spt+multiverse!3m6!1s0x2e69f5caa2f92667:0x8ffaa52281733371!8m2!3d-6.2080777!4d106.8204522!15sCg1wdCBtdWx0aXZlcnNlkgEQc29mdHdhcmVfY29tcGFueeABAA!16s%2Fg%2F11jt04mf8x'} rel="noopener">
                     <Image src={map} alt="connect" width={35} height={35} />
                   </Link>
                 </div>
