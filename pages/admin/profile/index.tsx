43ef38570d8eb73c090e0f3ce735df639ec54fb8
alvin <alvinecom2018@gmail.com>
2023-01-11 09:07:58 +0700

    profiles

--- pages/admin/profile/index.tsx
+++ pages/admin/profile/index.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { Form } from 'react-bootstrap'
+import Swal from 'sweetalert2'
 import Input from '../../../components/Input/Input'
 import Layout from '../../../components/Layout'
+import { Config } from '../../../config'
 
 export default function Profile() {
+    const [payload, setPayload] = useState<any>()
+    const handleChange = (e: any) => {
+        setPayload({ ...payload, [e.target.name]: e.target.value })
+    }
+
+    const getData = async () => {
+        try {
+            const result = await axios.get(`${Config.base_url_api.base}/profiles/`)
+            if (result.data.result?.length > 0) {
+                setPayload(result.data.result[0])
+            }
+        } catch (error) {
+            console.log(error);
+            Swal.fire({
+                text: 'Internal server error',
+                icon: 'error'
+            })
+        }
+    }
+
+    const update = async () => {
+        try {
+            const payloadData = {
+                ...payload
+            }
+            const result = await axios.patch(`${Config.base_url_api.base}/profiles/?id=${payload?.ID}`, payloadData)
+            getData()
+            Swal.fire({
+                text: 'Success Update Data',
+                icon: 'success'
+            })
+        } catch (error) {
+            console.log(error);
+            Swal.fire({
+                text: 'Internal server error',
+                icon: 'error'
+            })
+        }
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
     return (
         <div>
             <Layout>
@@ -12,44 +58,42 @@ export default function Profile() {
                         <h3>Profil Perusahaan</h3>
                     </div>
                     <div>
-                        <Form>
-                            <div className='row'>
-                                <div className='col'>
-                                    <Input label='Nama Perusahaan' placeholder='Masukkan nama perusahaan' />
-                                </div>
-                                <div className='col'>
-                                    <Input label='Alamat Perusahaan' placeholder='Masukkan alamat perusahaan' />
-                                </div>
+                        <div className='row'>
+                            <div className='col'>
+                                <Input onChange={handleChange} defaultValue={payload?.Name || ""} name="name" label='Nama Perusahaan' placeholder='Masukkan nama perusahaan' />
                             </div>
-                            <div className='row'>
-                                <div className='col'>
-                                    <Input label='No Telepon / Handphone' placeholder='Masukkan no telepon / handphone' type={'number'} />
-                                </div>
-                                <div className='col'>
-                                    <Input label='Email Perusahaan' placeholder='Masukkan email perusahaan' type={'email'} />
-                                </div>
+                            <div className='col'>
+                                <Input onChange={handleChange} defaultValue={payload?.Address || ""} name="address" label='Alamat Perusahaan' placeholder='Masukkan alamat perusahaan' />
                             </div>
-                            <div className='row'>
-                                <div className='col'>
-                                    <Input label='Latitude' placeholder='-6092829' type={'number'} />
-                                </div>
-                                <div className='col'>
-                                    <Input label='Longitude' placeholder='0987678' type={'number'} />
-                                </div>
+                        </div>
+                        <div className='row'>
+                            <div className='col'>
+                                <Input onChange={handleChange} defaultValue={payload?.Phone || ""} name="phone" label='No Telepon / Handphone' placeholder='Masukkan no telepon / handphone' />
                             </div>
-                            <div className='row mt-2'>
-                                <div className='col'>
+                            <div className='col'>
+                                <Input onChange={handleChange} defaultValue={payload?.Email || ""} name="email" label='Email Perusahaan' placeholder='Masukkan email perusahaan' type={'email'} />
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='col'>
+                                <Input onChange={handleChange} defaultValue={payload?.Lat || ""} name="lat" label='Latitude' placeholder='-6092829' />
+                            </div>
+                            <div className='col'>
+                                <Input onChange={handleChange} defaultValue={payload?.Long || ""} name="long" label='Longitude' placeholder='0987678' />
+                            </div>
+                        </div>
+                        <div className='row mt-2'>
+                            <div className='col'>
 
-                                </div>
-                                <div className='col'>
-                                    <div className='d-flex justify-content-end'>
-                                        <button className='btn btn-success w-50'>
-                                            Update
-                                        </button>
-                                    </div>
+                            </div>
+                            <div className='col'>
+                                <div className='d-flex justify-content-end'>
+                                    <button onClick={update} className='btn btn-success w-50'>
+                                        Update
+                                    </button>
                                 </div>
                             </div>
-                        </Form>
+                        </div>
                     </div>
                 </div>
             </Layout>
